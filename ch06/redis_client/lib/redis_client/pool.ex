99aebc02cb82886d857c9248f79484ecad955410
Meraj Molla <meraj.enigma@gmail.com>
2025-06-20 08:39:34 -0400

    client with poolboy

---
+++ ch06/redis_client/lib/redis_client/pool.ex
@@ -0,0 +1,14 @@
+defmodule RedisClient.Pool do
+  @moduledoc false
+
+  def start_link(worker_args) do
+    pool_args = [worker_module: RedisClientQueue, size: 5]
+    :poolboy.start_link(pool_args, worker_args)
+  end
+
+  def command(pool, command) do
+    :poolboy.transaction(pool, fn client ->
+      RedisClientQueue.command(client, command)
+    end)
+  end
+end
